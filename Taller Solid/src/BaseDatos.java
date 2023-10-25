bdbd629df2592e99f170ac476877d41402c66f14
Ariel-Vargas <132379170+Ariel-Vargas@users.noreply.github.com>
2023-10-25 16:37:09 -0500

    Update BaseDatos.java

--- Taller Solid/src/BaseDatos.java
+++ Taller Solid/src/BaseDatos.java
@@ -1,6 +1,8 @@
 public interface BaseDatos{
-
-  public void insertUserInDatabase(User user){
-        // Insert user in database
-    }
+  
+  public void insert(String statement){}
+  public void select(String statement){}
+  public void delete(String statement){}
+  public void update(String statement){}
+  // More Methods
 }
