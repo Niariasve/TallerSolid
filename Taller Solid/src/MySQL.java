b4e62561cd93f4f11e08657525bc9a078357bd2c
Ariel-Vargas <132379170+Ariel-Vargas@users.noreply.github.com>
2023-10-25 16:36:08 -0500

    Update MySQL.java

--- Taller Solid/src/MySQL.java
+++ Taller Solid/src/MySQL.java
@@ -1,7 +1,11 @@
 public class MySQL implements BaseDatos{
+    @override
     public void insert(String statement){}
+    @override
     public void select(String statement){}
+    @override
     public void delete(String statement){}
+    @override
     public void update(String statement){}
     // More Methods
 }
