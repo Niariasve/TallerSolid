4b70ae8e0865941baa79d7e2c848f08ba5d21916
Johrespi <139030531+Johrespi@users.noreply.github.com>
2023-10-25 19:18:30 -0500

    Update LogIn.java

--- Taller Solid/src/LogIn.java
+++ Taller Solid/src/LogIn.java
@@ -1,5 +1,5 @@
-public class LogIn {
-    public void log (User user) extends insertUser {
+public class LogIn extends insertUser {
+    public void log (User user) {
         System.out.println("Has access to the website");
         insertUserInDatabase(user);
         // Logic
